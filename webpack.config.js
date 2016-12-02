da15233f463985930d39bfe77c1248c51af4e3d1
ALI1995 <ali1364137942@outlook.com>
2016-12-02 23:33:29 +0800

    add webpack-merge,webpack-validator

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,16 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const merge = require('webpack-merge');
+const validate = require('webpack-validator');
+
+
 const PATHS = {
     app: path.join(__dirname, 'app'),
     build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+const common =  {
     // Entry accepts a path or an object of entries.
     // We'll be using the latter form given it's
     // convenient with more complex configurations.
@@ -26,3 +30,15 @@ module.exports = {
         })
     ]
 };
+
+var config;
+
+//探测npm是如何运行的
+switch(process.env.npm_lifecycle_event) {
+    case 'build':
+        config = merge(common, {});
+        break;
+    default:
+        config = merge(common, {});
+}
+module.exports = validate(config);
