2bb5289135715f4cdc4c92152d3bed41628253b2
ALI1995 <ali1364137942@outlook.com>
2016-12-02 23:29:36 +0800

    webpack init

---
+++ app/component.js
@@ -0,0 +1,10 @@
+/**
+ * Created by 子健 on 2016-12-2.
+ */
+module.exports = function () {
+    var element = document.createElement('h1');
+
+    element.innerHTML = 'Hello world';
+
+    return element;
+};
---
+++ app/index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by 子健 on 2016-12-2.
+ */
+var component = require('./component');
+
+document.body.appendChild(component());
